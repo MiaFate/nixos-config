f4f8a0da620c8551778ef2b456d2abf14e75b492
miafate <miapurplefroot@gmail.com>
2026-05-08 06:39:44 -0300

    chore: simplify XDG_CURRENT_DESKTOP to niri for testing

--- configuration.nix
+++ configuration.nix
@@ -175,7 +175,7 @@
   environment.sessionVariables = {
 	WLR_NO_HARDWARE_CURSORS = "1";
   	NIXOS_OZONE_WL = "1";
-        XDG_CURRENT_DESKTOP = "niri:GNOME";   # Importante para que los portales sepan quién manda
+        XDG_CURRENT_DESKTOP = "niri";   # Simplificado para evitar conflictos de sesión
         XDG_SESSION_TYPE = "wayland";
         GBM_BACKEND = "nvidia-drm";
         __GLX_VENDOR_LIBRARY_NAME = "nvidia";
