c4e8fd5cad5f57f9d3a2a29667a997737e697713
miafate <miapurplefroot@gmail.com>
2026-05-12 03:53:12 -0300

    refactor: move session-specific environment variables to compositor configs

--- configuration.nix
+++ configuration.nix
@@ -241,8 +241,6 @@
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
-    XDG_CURRENT_DESKTOP = "niri:GNOME";
-    XDG_SESSION_TYPE = "wayland";
     GBM_BACKEND = "nvidia-drm";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     LIBVA_DRIVER_NAME = "nvidia";
